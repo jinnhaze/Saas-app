b49495499d144a037b7ac2cee703441ec318c3c2
jinnhaze <kannankashinadh@gmail.com>
2026-04-09 23:18:29 +0530

    ai models added

--- backend/ai_engine/models.py
+++ backend/ai_engine/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Generationtype(models.TextChoices):
+    TEXT = 'text','Text Generator'
+    ARTICLE = 'article','Article Writer'
+    RESUME_REVIEW = 'resume','Resume Review'
+    IMAGE_GENERATE = 'image_generate','Image Generator'
+    IMAGE_EDIT = 'image_edit','Image Editor (remove object/background)'
+
+
+
+class AIGeneration(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='ai_generations')
+    generation_type = models.CharField(max_length=20,choices=Generationtype.choices)
+    prompt = models.TextField()
+    result_text = models.TextField(blank=True,null=True)
+    result_image_url = models.URLField(blank=True,null=True)
+
+    tokens_used = models.PositiveIntegerField(default=0)
+    cost_in_credits = models.PositiveIntegerField(default=1)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta :
+        
