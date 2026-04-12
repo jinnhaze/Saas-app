c757b471a332ad0ef90181a1d4deb6c0c3581b86
jinnhaze <kannankashinadh@gmail.com>
2026-04-12 23:21:47 +0530

    halfway om completong the subscription plan using razorpay

--- backend/subscriptions/models.py
+++ backend/subscriptions/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+
+
+class SubscriptionPlan(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    credits_per_month = 
